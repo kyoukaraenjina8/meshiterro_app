e3ef27687b57981a0fa4d4fbb17c9df2d796b0d4
Kazui Fukushima <fronta.kazui.1429@gmail.com>
2023-04-20 22:02:59 +0000

    process1 4-9コントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'postimage/new'
+  get 'postimage/index'
+  get 'postimage/show'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: "homes#top"
-  get "/homes/about" => "homes#about", as: "about" 正解
+  get "/homes/about" => "homes#about", as: "about" 
 
 end
